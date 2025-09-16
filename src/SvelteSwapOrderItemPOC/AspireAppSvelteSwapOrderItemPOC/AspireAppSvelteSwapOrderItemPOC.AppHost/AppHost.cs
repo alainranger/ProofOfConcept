687dc5a68e99f0cfb9131a0902c8d757e2b02e38
Alain Ranger <alainranger@gmail.com>
2025-09-16 08:00:50 -0400

    feat: enhance application setup with Node.js integration and dynamic Vite configuration

--- src/SvelteSwapOrderItemPOC/AspireAppSvelteSwapOrderItemPOC/AspireAppSvelteSwapOrderItemPOC.AppHost/AppHost.cs
+++ src/SvelteSwapOrderItemPOC/AspireAppSvelteSwapOrderItemPOC/AspireAppSvelteSwapOrderItemPOC.AppHost/AppHost.cs
@@ -3,10 +3,17 @@ var builder = DistributedApplication.CreateBuilder(args);
 var postgres = builder.AddPostgres("postgres");
 var postgresdb = postgres.AddDatabase("postgresdb");
 
-builder.AddProject<Projects.AspireAppSvelteSwapOrderItemPOC_WebApi>("aspireappsvelteswaporderitempoc-webapi")
+var webpi = builder.AddProject<Projects.AspireAppSvelteSwapOrderItemPOC_WebApi>("webapi")
 	.WithReference(postgresdb)
 	.WaitFor(postgresdb);
 
+//frontend nodejs from AspireAppSvelteSwapOrderItemPOC.Svelte
+builder.AddNpmApp("frontend", "../AspireAppSvelteSwapOrderItemPOC.Svelte", "dev")
+	.WithReference(webpi)
+	.WaitFor(webpi)
+	.WithHttpEndpoint(env: "VITE_PORT")
+	.WithExternalHttpEndpoints()
+	.PublishAsDockerFile();
 
 
 builder.Build().Run();
--- src/SvelteSwapOrderItemPOC/AspireAppSvelteSwapOrderItemPOC/AspireAppSvelteSwapOrderItemPOC.Svelte/vite.config.ts
+++ src/SvelteSwapOrderItemPOC/AspireAppSvelteSwapOrderItemPOC/AspireAppSvelteSwapOrderItemPOC.Svelte/vite.config.ts
@@ -1,6 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		plugins: [sveltekit()],
+		server: {
+			port: parseInt(env.VITE_PORT)
+		},
+		build: {
+			outDir: 'dist',
+			rollupOptions: {
+				input: './index.html'
+			}
+		}
+	}
+})
